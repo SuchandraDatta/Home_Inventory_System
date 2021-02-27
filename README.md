0bfa112a2233ea05f3b5a521d62ebc6de07d366d
SuchandraDatta <41965125+SuchandraDatta@users.noreply.github.com>
2021-02-27 20:33:22 +0530

    Final commit to Version 1.0

---
+++ README.md
@@ -0,0 +1,3 @@
+**Home Inventory Project**
+
+A web based application thats supports CRUD operations like saving product details, searching for products based on product name, category, aggregation on product price and quantity, filtering and sorting on price, quantity, and expiry date, deleting specific products and updating details of products. Nodejs(Express) and Firebase Firestore has been used.
--- index.js
+++ index.js
@@ -10,25 +10,26 @@ app.set("view engine", "ejs")
 app.use("/assets", express.static("assets"))
 app.use(objForUrlencoded)
 
-app.get("/", (req,res,next)=>{
+app.get("/", (req,res,next)=>{//Show the homepage
 	res.render("homepage")
 })
-app.get("/save_data.ejs", (req,res,next)=>{
+app.get("/save_data.ejs", (req,res,next)=>{//Show the form for saving data
 	res.render("save_data")
 })
-app.get("/search_data.ejs", (req,res,next)=>{
+app.get("/search_data.ejs", (req,res,next)=>{//Show the form for searching data
 	res.render("search_data")
 })
-app.get("/delete", (req,res) => {
+app.get("/delete", (req,res) => {//Delete data and show the form for searching data
 	console.log("ID TO DELETE: ", req.query.id)
 	firebase_functions.firebase_delete_data(req.query.category, res, req.query.id)
 })
 app.post("/successpage", objForUrlencoded, (req,res)=>{
+	//Success page after data is saved in the database
 	console.log(firebase_functions)
 	firebase_functions.firebase_save_data(req.body, res)
-	//res.render("successpage")
 })
 app.post("/search_data_output.ejs", (req, res, next)=>{
+	//Perform search/sort/filtering/aggregation
 	category = req.body["cat"]
 	filter_criteria = req.body["filter"]
 	aggregate_over = req.body["aggregate_over"]
@@ -37,6 +38,7 @@ app.post("/search_data_output.ejs", (req, res, next)=>{
 	firebase_functions.firebase_retrieve_data(category,res,filter_criteria, aggregate_over, prodName, exprInMonths)
 })
 app.post("/update", objForUrlencoded, (req,res) => {
+	//Update the data and show the form for search data
 	console.log(req.body)
 	firebase_functions.firebase_update_data(req.body["category"], res, req.body)
 })
